3298442cac4ae7fb14c9a7cd6f0c140c80d08c89
suma-2004 <142984554+suma-2004@users.noreply.github.com>
2023-09-27 16:03:34 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@
 
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, suma irritatinf fellow"); 
+        System.out.println("Hello, suma irritating fellow"); 
     }
 }
