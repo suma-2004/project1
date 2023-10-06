80ec05f28b004fae2fd9e84b2bca1cfa9e67db90
suma-2004 <142984554+suma-2004@users.noreply.github.com>
2023-10-06 11:10:22 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
-
-class Hello {
-    public static void main(String[] args) {
-        System.out.println("testing the intigration"); 
-    }
-}
+class Hello {  
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int number=5;//It is the number to calculate factorial    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+}  
