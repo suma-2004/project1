6fd1c7e93601d836c2e705ad0c6edcd90308cf9e
suma-2004 <142984554+suma-2004@users.noreply.github.com>
2023-10-06 11:17:27 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,29 +1,19 @@
 public class Hello {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter a non-negative integer: ");
-        int n = scanner.nextInt();
-        
-        if (n < 0) {
-            System.out.println("Factorial is undefined for negative numbers.");
-        } else {
-            long factorial = calculateFactorial(n);
-            System.out.println("The factorial of " + n + " is " + factorial);
-        }
-        
-        scanner.close();
+        int number = 5; // Change this to the desired number for which you want to calculate the factorial.
+        long factorial = calculateFactorial(number);
+        System.out.println("The factorial of " + number + " is " + factorial);
     }
-    
+
     public static long calculateFactorial(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        } else {
-            long result = 1;
-            for (int i = 2; i <= n; i++) {
-                result *= i;
-            }
-            return result;
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is undefined for negative numbers.");
         }
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
     }
 }
+
